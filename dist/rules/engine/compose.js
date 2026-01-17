2b5093dbb3eb033a94070bc1666abf3ed544ed72
iamspruce <iamspruce8@gmail.com>
2026-01-17 08:05:53 +0100

    chore: stop tracking dist folder

--- dist/rules/engine/compose.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.composeScoring = exports.composeTransforms = exports.composeFilters = void 0;
-const composeFilters = (...rules) => rules;
-exports.composeFilters = composeFilters;
-const composeTransforms = (...rules) => rules;
-exports.composeTransforms = composeTransforms;
-const composeScoring = (...rules) => rules;
-exports.composeScoring = composeScoring;
--- dist/rules/selection/pickTopN.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pickTopN = pickTopN;
-function pickTopN(items, limit) {
-    return [...items]
-        .sort((a, b) => b.score - a.score)
-        .slice(0, limit);
-}
